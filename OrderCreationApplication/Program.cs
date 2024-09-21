8be96e9e5bb936ee949e77401ef4e996e959360a
manjusa <thizismanju@mail.com>
2024-09-21 14:53:01 +1000

    staged changes

---
+++ OrderCreationApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration configuration = builder.Build();
+var connectionString= configuration.GetConnectionString("ServiceBus");
+var queueName = "orderqueue";
+
+await SendOrdersAsync();
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+
+
+ async Task SendOrdersAsync()
+{
+    var client = new ServiceBusClient(connectionString);
+    ServiceBusSender sender = client.CreateSender(queueName);
+
+    for (int i = 1; i <= 5; i++)
+    {
+        var order = new Order
+        {
+            Product = $"Product-{i}",
+            Quantity = i * 5,
+            CustId =  $"Cust-{i}"
+        };
+
+        var message = new ServiceBusMessage(System.Text.Json.JsonSerializer.Serialize(order))
+        {
+            ContentType = "application/json"
+        };
+
+        await sender.SendMessageAsync(message);
+        Console.WriteLine($"Sent order: {System.Text.Json.JsonSerializer.Serialize(order)}");
+    }
+
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+public class Order
+{
+    public string Product { get; set; }
+    public int Quantity { get; set; }
+    public string CustId { get; set; }
+}
---
+++ OrderWebAPI/Controllers/OrderController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OrderWebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : Controller
+    {
+        [HttpPost]
+        public IActionResult ProcessOrder([FromBody] Order order)
+        {
+            // Process the order- Save to DB etc
+            return Ok($"Order for {order.Product} processed successfully.");
+        }
+        public class Order
+        {
+            public string Product { get; set; }
+            public int Quantity { get; set; }
+            public string CustomerId { get; set; }
+        }
+    }
+}
